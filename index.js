c49c6bd3de418f1a8f63017acb95f514a8ac486c
MissDine <enidnyatichi1212@gmail.com>
2022-06-01 08:48:17 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ for(let i = 0 ;i < 33;i--){
     console.log(i);
 }
 console.log("Happy birthday bebe,Yey,you are 33!");
+
+const names = ["Enid","Norbert","Moraa",",Marube"]
+for(let i = 0; i < names.length;i++){
+    console.log("Good morning",names[i]);
+}
