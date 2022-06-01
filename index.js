ce3c8d96a2d740835b665019f1577c95b61aa956
MissDine <enidnyatichi1212@gmail.com>
2022-06-01 08:43:35 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 for(let i = 0; i < 5; i++){
     console.log("in loop:", i);
 }
+console.log("Loop finished!");
+
+for(let i = 0 ;i < 33;i--){
+    console.log(i);
+}
+console.log("Happy birthday bebe,Yey,you are 33!");
