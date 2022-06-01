ead4f68ffe1fb503f24afef50a7b9e882830b77d
MissDine <enidnyatichi1212@gmail.com>
2022-06-01 09:15:29 +0300

    Feat:Edit

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ for(let i = 0; i < 5; i++){
 }
 console.log("Loop finished!");
 
-for(let i = 0 ;i < 33;i--){
+for(let i = 0 ;i < 33;i++){
     console.log(i);
 }
-console.log("Happy birthday bebe,Yey,you are 33!");
+console.log("Happy birthday bebe!Yey,you are 33!");
 
 const names = ["Enid","Norbert","Moraa",",Marube"]
 for(let i = 0; i < names.length;i++){
     console.log("Good morning",names[i]);
 }
+
+
+// WHILE LOOP
+let i = 0
+while (i < 26) {
+    console.log(i);
+    i++
+}
+console.log("You are 25!");
